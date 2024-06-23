3e0c054b118a775a7b49971a0b8cc92214dddd40
Coderyounes <younesanas2021@gmail.com>
2024-06-23 14:24:44 +0100

    feat: enhance freelance model & add some fields & validations

--- models/freelance.js
+++ models/freelance.js
@@ -2,22 +2,54 @@ const mongoose = require('mongoose');
 
 const freelanceSchema = new mongoose.Schema({
   _id: { type: String, required: true, unique: true },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  address: { type: String },
-  country: { type: String, required: true },
+  firstname: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 30,
+  },
+  address: {
+    type: String,
+    min: 10,
+    max: 100,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  usertype: {
+    type: String,
+    required: true,
+    enum: ['freelance', 'employer'],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
   city: { type: String },
   photo: { type: Buffer },
   skills: { type: String },
-  usertype: { type: String, required: true },
+
 });
 
 const Freelance = mongoose.model('Freelance', freelanceSchema);
 
 module.exports = Freelance;
 
-// TODO: store emails
-// TODO: use validations to validate some Data
 // TODO: Complete other Models Definition & Validation
 // TODO: Setup Basic CRUD operation for Freelancer
 // TODO: Setup Basic CRUD operation for employer
