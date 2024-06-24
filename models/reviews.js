1029e369ac736d09a9c5ca41bf45ba7f6aa3fd86
Coderyounes <younesanas2021@gmail.com>
2024-06-24 11:53:41 +0100

    feat: enhance review model & make handle both review from employer or freelancer

--- models/reviews.js
+++ models/reviews.js
@@ -1,21 +1,38 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-  reviews: {
-    name: {
-      type: String,
-      required: true,
-    },
-    comment: {
-      type: String,
-      required: true,
-    },
-    rating: {
-      type: Number, min: 0, max: 5, required: true,
-    },
+  reviewer: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    refPath: 'onModel',
+  },
+  reviewee: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    refPath: 'onModel',
+  },
+  onModel: {
+    type: String,
+    required: true,
+    enum: ['freelancer', 'employer'],
+  },
+  projectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'projects',
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: true,
   },
 });
 
-const reviews = mongoose.model('Reviews', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
-module.exports = reviews;
+module.exports = Review;
