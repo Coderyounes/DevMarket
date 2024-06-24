f036989c10250ec2a7c1319d3dc89102b8bf769f
Coderyounes <younesanas2021@gmail.com>
2024-06-24 19:06:35 +0100

    feat: Create Employer Profile Basic CRUD

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../controllers/UserController');
 const freelanceController = require('../controllers/freelanceController');
 const visitorController = require('../controllers/visitorController');
+const employerController = require('../controllers/EmployerController');
 const { authenticate } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -18,11 +19,13 @@ router.post('/resetpassword'); // Later
 router.post('/sendverification'); // Later
 
 // Freelance routes
-router.get('/userProfile', authenticate, freelanceController.userProfile);
-router.put('/updateProfile', authenticate, freelanceController.updateProfile);
-router.delete('/deleteProfile', authenticate, freelanceController.deleteProfile);
+router.get('/freelance/userProfile', authenticate, freelanceController.userProfile);
+router.put('/freelance//updateProfile', authenticate, freelanceController.updateProfile);
+router.delete('/freelance//deleteProfile', authenticate, freelanceController.deleteProfile);
 // Employer routes
-
+router.get('/employer/Profile', authenticate, employerController.employerProfile);
+router.put('/employer/updateProfile', authenticate, employerController.updateProfile);
+router.delete('/employer/deleteProfile', authenticate, employerController.deleteProfile);
 // unauth user routes
 router.get('/getProfile/:id', visitorController.getProfile);
 router.get('/allProfile', visitorController.allProfiles);
