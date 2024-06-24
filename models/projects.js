fba34ea09c1f4f13e89eaee43062f2bb6cfd86cb
Coderyounes <younesanas2021@gmail.com>
2024-06-24 01:13:18 +0100

    feat: add some validations for project model

--- models/projects.js
+++ models/projects.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-  title: {},
-  description: {},
-  price: {},
-  delay: {},
+  title: {
+    type: String,
+    required: true,
+    min: 10,
+    max: 150,
+  },
+  description: {
+    type: String,
+    required: true,
+    min: 100,
+    max: 3000,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  delay: {
+    type: String,
+    required: true,
+  },
   employer: { type: mongoose.Schema.Types.ObjectId, ref: 'employers' },
   freelance: { type: mongoose.Schema.Types.ObjectId, ref: 'freelances', default: null },
 });
