c251b5499d70d869928d3360c67da5bc53009b8d
escaper <assaoui54@gmail.com>
2024-07-02 18:31:13 +0100

    fix: typos corrected

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -4,8 +4,8 @@ export default function AboutUs() {
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="text-center">
-            <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">
-              Our team members
+            <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl capitalize">
+              Founders
             </h2>
           </div>
 
@@ -48,26 +48,19 @@ export default function AboutUs() {
               Welcome to DevMarket, a dynamic platform where freelancers can
               discover diverse missions posted by employers. Our goal is to
               bridge the gap between skilled freelancers and businesses in need
-              of their expertise.
+              of their expertise. At the heart of this innovative project are
+              two passionate developers, Younes and Mohamed, who have poured
+              their skills and dedication into making DevMarket a reality.
             </p>
+            <br />
+
+            <h1 className="text-xl font-bold">
+              Younes - Backend Developer Younes
+            </h1>
             <p>
-              Welcome to DevMarket, a dynamic platform where freelancers can
-              discover diverse missions posted by employers. Our goal is to
-              bridge the gap between skilled freelancers and businesses in need
-              of their expertise.
-            </p>
-            <p>
-              At the heart of this innovative project are two passionate
-              developers, Younes and Mohamed, who have poured their skills and
-              dedication into making DevMarket a reality.
-            </p>
-            <br/>
-            <h1 className="text-xl font-bold">Younes - Backend Developer Younes</h1>
-            
-            <p>
-              Our talented backend developer, is the architect behind the robust
-              and efficient server-side operations of DevMarket.With a strong
-              foundation in database management, API development, and
+              Younes, our talented backend developer, is the architect behind
+              the robust and efficient server-side operations of DevMarket. With
+              a strong foundation in database management, API development, and
               server-side scripting, Younes ensures that our platform runs
               smoothly and securely. His motivation for joining this project
               stems from a desire to create a seamless user experience for both
@@ -77,41 +70,43 @@ export default function AboutUs() {
               DevMarket is scalable, reliable, and ready to handle the needs of
               a growing community.
             </p>
-            <br/>
-            <h1 className="text-xl font-bold">Mohamed - Frontend Developer Mohamed</h1>
-            
+            <br />
+
+            <h1 className="text-xl font-bold">
+              Mohamed - Frontend Developer Mohamed
+            </h1>
+
             <p>
-              our creative frontend developer, is the visionary behind the user
-              interface and user experience design of DevMarket. With a keen eye
-              for detail and a passion for crafting intuitive and engaging user
-              experiences, Mohamed has brought the platform to life through his
-              expertise in HTML, CSS, JavaScript, and modern frontend
-              frameworks.
+              Mohamed, our creative frontend developer, is the visionary behind
+              the user interface and user experience design of DevMarket. With a
+              keen eye for detail and a passion for crafting intuitive and
+              engaging user experiences, Mohamed has brought the platform to
+              life through his expertise in HTML, CSS, JavaScript, and modern
+              frontend frameworks. His motivation lies in his commitment to
+              creating a platform that is not only functional but also visually
+              appealing and easy to navigate. As his final project for the ALX
+              Software Engineering program, Mohamed has worked tirelessly to
+              ensure that DevMarket offers a smooth and enjoyable experience for
+              all users.
             </p>
+            <br />
+            <h1 className="text-xl font-bold">Our motivation</h1>
             <p>
-              As part of their final project for the ALX Software Engineering
-              program, they were driven by a shared vision to create a
-              meaningful and impactful platform that could serve the global
-              freelance community. Their dedication to excellence and their
-              passion for technology have fueled their efforts to bring
-              DevMarket to life. We believe that DevMarket will not only connect
-              freelancers with valuable opportunities but also empower them to
-              showcase their skills and grow their careers. We are excited to
-              share this platform with you and look forward to seeing the
-              incredible collaborations and projects that will emerge from our
-              community. Thank you for being a part of DevMarket. Together, we
-              can create a vibrant and thriving ecosystem for freelancers and
-              employers alike.
+              The journey of building DevMarket has been both challenging and
+              rewarding for Younes and Mohamed. As part of their final project
+              for the ALX Software Engineering program, they were driven by a
+              shared vision to create a meaningful and impactful platform that
+              could serve the global freelance community. Their dedication to
+              excellence and their passion for technology have fueled their
+              efforts to bring DevMarket to life.
             </p>
 
             <p>
-              His motivation lies in his commitment to creating a platform that
-              is not only functional but also visually appealing and easy to
-              navigate. As his final project for the ALX Software Engineering
-              program, Mohamed has worked tirelessly to ensure that DevMarket
-              offers a smooth and enjoyable experience for all users. Our
-              Motivation The journey of building DevMarket has been both
-              challenging and rewarding for Younes and Mohamed.
+              We believe that DevMarket will not only connect freelancers with
+              valuable opportunities but also empower them to showcase their
+              skills and grow their careers. We are excited to share this
+              platform with you and look forward to seeing the incredible
+              collaborations and projects that will emerge from our community.
             </p>
           </article>
         </div>
